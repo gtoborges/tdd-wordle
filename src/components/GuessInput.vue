f2515de6901fec53e269f13a7d146a5965965c82
Augusto <gtoborges@gmail.com>
2024-04-15 11:54:45 -0300

    add styles to the wordleboard

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -31,6 +31,15 @@ const onSubmit = () => {
 </script>
 
 <template>
+  <ul class="word">
+    <li v-for="(letter, index) in formattedGuessInProgress.padEnd(WORD_SIZE, ' ')"
+      :key="`${letter}-${index}`"
+      :data-letter="letter"
+      class="letter"
+      v-text="letter"
+    />
+  </ul>
+
   <input v-model="formattedGuessInProgress"
     :maxlength="WORD_SIZE"
     type="text" 
@@ -38,3 +47,43 @@ const onSubmit = () => {
   >
 </template>
 
+<style scoped>
+input {
+  position: absolute;
+  opacity: 0;
+}
+
+.word {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 0.25rem;
+}
+
+.letter {
+  background-color: white;
+  border: 1px solid hsl(0, 0%, 70%);
+  width: 5rem;
+  height: 5rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: bolder;
+}
+
+li:not([data-letter=" "]) {
+  animation: pop 100ms;
+}
+
+@keyframes pop {
+  0% {
+    transform: scale(1);
+  }
+  
+  50% {
+    transform: scale(1.4);
+  }
+}
+</style>
